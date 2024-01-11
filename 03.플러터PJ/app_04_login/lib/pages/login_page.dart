52b11ce607d421cb64b4d97f512d13542f22bb71
minjxx <qnsdbtlrp1018@naver.com>
2024-01-11 12:01:31 +0900

    update

--- 03.플러터PJ/app_04_login/lib/pages/login_page.dart
+++ 03.플러터PJ/app_04_login/lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 // 로그인 페이지 /////
 
+import 'package:app_04_login/components/custom_form.dart';
 import 'package:flutter/material.dart';
 // 로고위젯추가
 import 'package:app_04_login/components/logo.dart';
@@ -14,27 +15,18 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       // 리스트뷰를 써야만 스크롤을 사용하여
       // 하단 입력키보드가 올라올때 대비할 수 있다!
-      body: ListView(
-        children: [
-          SizedBox(height: xlargeGap),
-          // 로고보이기
-          Logo('Login'),
-          SizedBox(height: xlargeGap),
-          // 입력폼양식위젯(폼요소위젯포함)
-          
-
-          // 임시이동버튼
-          // TextButton(
-          //   // 대상을 누를때 발생
-          //   onPressed: () {
-          //     // debugPrint('나눌렸어?');
-          //     // 라우터이동은 Navigator
-          //     // pushNamed() 메서드로!!!
-          //     Navigator.pushNamed(context, '/home');
-          //   },
-          //   child: Text('Go to Home!'),
-          // ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            SizedBox(height: xlargeGap),
+            // 로고보이기
+            Logo('Login'),
+            SizedBox(height: xlargeGap),
+            // 2. 입력폼양식위젯(폼요소위젯포함)
+            CustomForm(),
+          ],
+        ),
       ),
     );
   }
